6cbad027753554c62fef4d8cc5cfc04da3a667bb
hungbin0209 <rename0209@gmail.com>
2022-12-09 11:04:34 +0700

    xong phần đăng nhập

--- DatGiaoThucAn/Program.cs
+++ DatGiaoThucAn/Program.cs
@@ -15,9 +15,9 @@ namespace DatGiaoThucAn
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new DangNhap());
+            Application.Run(new DangNhap());
             //Application.Run(new TaiXe_Main());
-            Application.Run(new KhachHang_Main());
+            //Application.Run(new KhachHang_Main());
             //Application.Run(new Form_AdminMain());
             //Application.Run(new Form_mainNhanVien());
         }
