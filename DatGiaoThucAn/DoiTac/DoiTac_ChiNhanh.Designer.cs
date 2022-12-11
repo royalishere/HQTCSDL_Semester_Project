6030e75db963753c8f4dc9cc579062b73cffb748
QUANG NGUYỄN ĐỨC <20120359@student.hcmus.edu.vn>
2022-12-11 12:41:51 +0700

    Add DoiTac_ChiNhanh

---
+++ DatGiaoThucAn/DoiTac/DoiTac_ChiNhanh.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace DatGiaoThucAn.DoiTac
+{
+    partial class DoiTac_ChiNhanh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_TenChiNhanh = new System.Windows.Forms.TextBox();
+            this.tb_DiaChi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgv_ChiNhanh = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ChiNhanh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(289, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHI NHÁNH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(151, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(519, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Địa chỉ";
+            // 
+            // tb_TenChiNhanh
+            // 
+            this.tb_TenChiNhanh.Location = new System.Drawing.Point(122, 94);
+            this.tb_TenChiNhanh.Name = "tb_TenChiNhanh";
+            this.tb_TenChiNhanh.Size = new System.Drawing.Size(100, 23);
+            this.tb_TenChiNhanh.TabIndex = 3;
+            // 
+            // tb_DiaChi
+            // 
+            this.tb_DiaChi.Location = new System.Drawing.Point(502, 94);
+            this.tb_DiaChi.Name = "tb_DiaChi";
+            this.tb_DiaChi.Size = new System.Drawing.Size(100, 23);
+            this.tb_DiaChi.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(78, 27);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(240, 27);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Size = new System.Drawing.Size(75, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(414, 27);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Size = new System.Drawing.Size(87, 38);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Cập nhật";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(577, 27);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Size = new System.Drawing.Size(75, 38);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Lưu";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tb_DiaChi);
+            this.panel1.Controls.Add(this.tb_TenChiNhanh);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(730, 146);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(0, 351);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(730, 96);
+            this.panel2.TabIndex = 10;
+            // 
+            // dgv_ChiNhanh
+            // 
+            this.dgv_ChiNhanh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ChiNhanh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_ChiNhanh.Location = new System.Drawing.Point(0, 0);
+            this.dgv_ChiNhanh.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgv_ChiNhanh.Name = "dgv_ChiNhanh";
+            this.dgv_ChiNhanh.RowHeadersWidth = 51;
+            this.dgv_ChiNhanh.RowTemplate.Height = 25;
+            this.dgv_ChiNhanh.Size = new System.Drawing.Size(729, 446);
+            this.dgv_ChiNhanh.TabIndex = 1;
+            this.dgv_ChiNhanh.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_ChiNhanh_CellContentClick);
+            // 
+            // DoiTac_ChiNhanh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 446);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgv_ChiNhanh);
+            this.Name = "DoiTac_ChiNhanh";
+            this.Text = "DoiTac_ChiNhanh";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ChiNhanh)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tb_TenChiNhanh;
+        private TextBox tb_DiaChi;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Panel panel1;
+        private Panel panel2;
+        private DataGridView dgv_ChiNhanh;
+    }
+}
---
+++ DatGiaoThucAn/DoiTac/DoiTac_ChiNhanh.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatGiaoThucAn.DoiTac
+{
+    public partial class DoiTac_ChiNhanh : Form
+    {
+        public DoiTac_ChiNhanh()
+        {
+            InitializeComponent();
+        }
+
+        private void dgv_ChiNhanh_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
